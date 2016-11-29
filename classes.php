c0250301c0b450bde2353b57c6708b905c6878d7
Mahlknecht Giovanni <giovanni.mahlknecht@inf.unibz.it>
2016-11-29 14:38:21 +0100

    input_filter for GET/POST

--- classes.php
+++ classes.php
@@ -16,28 +16,28 @@ $c = '';
 
 $header = get_string('classespagetitle', 'block_classmanager');
 
-if (!isset($_GET['category']) and ! isset($_POST['category'])) {
+if (!filter_has_var(INPUT_GET, 'category') and ! filter_has_var(INPUT_POST, 'category')) {
     $c .= "no category";
 } else {
-    if (isset($_GET['category'])) {
-        $categoryid = $_GET['category'];
+    if (filter_has_var(INPUT_GET, 'category')) {
+        $categoryid = filter_input(INPUT_GET, 'category', FILTER_SANITIZE_NUMBER_INT);
     } else {
-        $categoryid = $_POST['category'];
+        $categoryid = filter_input(INPUT_POST, 'category', FILTER_SANITIZE_NUMBER_INT);
     }
     $context = context_coursecat::instance($categoryid);
     if (has_capability(PERMISSION, $context)) {
         $school = $DB->get_record('course_categories', array('id' => $categoryid));
-
         $PAGE->navbar->add(get_string('manage', 'block_classmanager') . ' ' . $school->name, new moodle_url($CFG->wwwroot . '/blocks/classmanager/admin.php?category=' . $categoryid));
         $PAGE->navbar->add(get_string('classes', 'block_classmanager'));
 
         $c .= get_string('classdescription', 'block_classmanager');
         $context = context_coursecat::instance($categoryid);
 
-        if (isset($_GET['action']) && $_GET['action'] == 'DELETE') {
-            $classes_match = $DB->get_records('cohort', array('id' => $_GET['classid'], 'contextid' => $context->id));
+        if (filter_has_var(INPUT_GET, 'action') && filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING) == 'DELETE') {
+            $classid = filter_input(INPUT_GET, 'classid', FILTER_SANITIZE_NUMBER_INT);
+            $classes_match = $DB->get_records('cohort', array('id' => $classid, 'contextid' => $context->id));
             if (is_array($classes_match) && count($classes_match) > 0) {
-                $DB->delete_records('cohort', array('id' => $_GET['classid']));
+                $DB->delete_records('cohort', array('id' => $classid));
                 $c .= "<br><b>" . get_string("groupdeleted", "block_classmanager") . "</b><br>";
             } else {
                 $c = "berechtigungsfehler";
