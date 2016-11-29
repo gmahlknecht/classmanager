c61fc8b1223297802a03dfa8e1738d01c2e11e5b
Mahlknecht Giovanni <giovanni.mahlknecht@inf.unibz.it>
2016-11-29 13:22:02 +0100

    correct indentation, encode Umlaute

--- lang/de/block_classmanager.php
+++ lang/de/block_classmanager.php
@@ -1,90 +1,93 @@
 <?php
-  $string['deleteusers']='Nutzer l&ouml;schen';
-  $string['deleteusersdescription']='Hier k&ouml;nnen sie die Nutzer ihrer Schule l&ouml;schen';
-  $string['deleteuserschoosecohort']='W&auml;hlen sie die Nutzergruppe';
-  $string['deleteusersdeletelink'] = 'Alle Nutzer dieser Klasse l&ouml;schen';
-  $string['deleteuserspagetitle'] = 'Nutzer l&ouml;schen';
-  $string['deleteusersmanydescription'] = 'Alle Nutzer einer Klasse l&ouml;schen';
-  $string['pluginname'] = 'Class Manager';
-  $string['adminpagetitle'] = 'Administration der Klassen und Schulen';
-  $string['manageschools'] = 'Schulen Verwalten';
-  $string['adminpagedescription'] = 'Hier können Sie Schulen erstellen/bearbeiten und ihnen Personen zuordnen, welche diese Benutzer bearbeiten dürfen';
-  $string['createdschools'] = 'Ihre Schulen:';
-  $string['shortname'] = 'Kurzname';
-  $string['name'] = 'Name';
-  $string['manager'] = 'Verwalter';
-  $string['edit'] = 'Bearbeiten';
-  $string['newschool'] = 'Eine neue Schule erstellen';
-  $string['noschool'] = 'Es wurde noch keine Schule erstellt';
-  $string['editschooltitle'] = 'Schule bearbeiten';
-  $string['schoolnotfound'] = 'Schule nicht gefunden';
-  $string['notalldata'] = 'Nicht alle Daten eingegeben';
-  $string['thisisanewschool'] = 'Neue Schule';
-  $string['saved'] = '&Auml;nderungen gespeichert';
-  $string['yourefromnoschool'] = 'Sie sind kein Verwalter einer Schule';
-  $string['manage'] = 'Verwalten:';
-  $string['managerpagedescription'] = 'Hier können Sie Klassen, Schüler und Lehrer Ihre Schule verwalten.';
-  $string['class'] = 'Klasse';
-  $string['classes'] = 'Klassen';
-  $string['students'] = 'Schüler und Lehrer';
-  $string['youhave'] = 'In Ihrem Bereich gibt es';
-  $string['manageclasses'] = 'Klassen verwalten';
-  $string['managestudents'] = 'Schüler und Lehrer verwalten';
-  $string['managerpagetitle'] = 'Administration Ihrer Schule';
-  $string['importstudents'] = 'Schüler und Lehrer importieren';
-  $string['theeaysiestway'] = 'Der einfachste Weg zum Starten';
-  $string['importpagetitle'] = 'Schüler und Lehrer importieren';
-  $string['import'] = 'Schüler und Lehrer importieren';
-  $string['importdescription'] = '<p>Auf dieser Seite können Sie eine Tabelle mit Benutzerdaten von Schülern und Lehrern hochladen. Bitten lesen Sie die Anleitung genau durch, bevor Sie den Upload starten.</p>
+$string['deleteusers'] = 'Nutzer l&ouml;schen';
+$string['deleteusersdescription'] = 'Hier k&ouml;nnen sie die Nutzer ihrer Schule '
+        . 'l&ouml;schen';
+$string['deleteuserschoosecohort'] = 'W&auml;hlen sie die Nutzergruppe';
+$string['deleteusersdeletelink'] = 'Alle Nutzer dieser Klasse l&ouml;schen';
+$string['deleteuserspagetitle'] = 'Nutzer l&ouml;schen';
+$string['deleteusersmanydescription'] = 'Alle Nutzer einer Klasse l&ouml;schen';
+$string['pluginname'] = 'Class Manager';
+$string['adminpagetitle'] = 'Administration der Klassen und Schulen';
+$string['manageschools'] = 'Schulen Verwalten';
+$string['adminpagedescription'] = 'Hier k&ouml;nnen Sie Schulen erstellen/bearbeiten '
+        . 'und ihnen Personen zuordnen, welche diese Benutzer bearbeiten d&uuml;rfen';
+$string['createdschools'] = 'Ihre Schulen:';
+$string['shortname'] = 'Kurzname';
+$string['name'] = 'Name';
+$string['manager'] = 'Verwalter';
+$string['edit'] = 'Bearbeiten';
+$string['newschool'] = 'Eine neue Schule erstellen';
+$string['noschool'] = 'Es wurde noch keine Schule erstellt';
+$string['editschooltitle'] = 'Schule bearbeiten';
+$string['schoolnotfound'] = 'Schule nicht gefunden';
+$string['notalldata'] = 'Nicht alle Daten eingegeben';
+$string['thisisanewschool'] = 'Neue Schule';
+$string['saved'] = '&Auml;nderungen gespeichert';
+$string['yourefromnoschool'] = 'Sie sind kein Verwalter einer Schule';
+$string['manage'] = 'Verwalten:';
+$string['managerpagedescription'] = 'Hier k&ouml;nnen Sie Klassen, Sch&uuml;ler und '
+        . 'Lehrer Ihre Schule verwalten.';
+$string['class'] = 'Klasse';
+$string['classes'] = 'Klassen';
+$string['students'] = 'Sch&uuml;ler und Lehrer';
+$string['youhave'] = 'In Ihrem Bereich gibt es';
+$string['manageclasses'] = 'Klassen verwalten';
+$string['managestudents'] = 'Sch&uuml;ler und Lehrer verwalten';
+$string['managerpagetitle'] = 'Administration Ihrer Schule';
+$string['importstudents'] = 'Sch&uuml;ler und Lehrer importieren';
+$string['theeaysiestway'] = 'Der einfachste Weg zum Starten';
+$string['importpagetitle'] = 'Sch&uuml;ler und Lehrer importieren';
+$string['import'] = 'Sch&uuml;ler und Lehrer importieren';
+$string['importdescription'] = '<p>Auf dieser Seite k&ouml;nnen Sie eine Tabelle mit Benutzerdaten von Sch&uuml;lern und Lehrern hochladen. Bitten lesen Sie die Anleitung genau durch, bevor Sie den Upload starten.</p>
 			<a style="text-align: center;" href="/moodle/blocks/classmanager/benutzertabelle_anleitung.pdf">Klicken Sie hier, um die Anleitung herunterzuladen.</a>
-			<p>Nach dem Import erhalten Sie eine Liste mit den Passwörtern. Bitte bewahren Sie diese sorgfältig auf! Diese Liste kann nur ein Mal heruntergeladen werden!</p>';
-  $string['iknowwhatido'] = 'Ich weiß was ich tue - Import starten';
-  $string['wrongdata'] = 'Falsche Daten';
-  $string['added'] = 'Hinzugefügt';
-  $string['classcreated'] = 'Klasse erstellt';
-  $string['useralreadyexists'] = 'Benutzer existiert bereits';
-  $string['classespagetitle'] = 'Verwaltung Ihrer Klassen';
-  $string['classdescription'] = 'Hier können Sie die Klassen Ihrer Schule verwalten.';
-  $string['editclassdescription'] = 'Hier können Sie eine Klasse bearbeiten.';
-  $string['noclassescreated'] = 'Es wurden noch keine Klassen erstellt.';
-  $string['newclass'] = 'Neue Klasse erstellen';
-  $string['numberofstudents'] = 'Anzahl eingeschriebener<br>Benutzer.';
-  $string['classnamenotconform'] = 'Klassenkurzname entspricht nicht den Regeln. Eine Zahl, dann Großbuchstaben. Ausnahmen: TE, PE';
-  $string['studentsdescription'] = 'Hier können Sie Schüler und Lehrer Ihrer Schule verwalten. Klicken Sie auf einen Namen um ihn zu bearbeiten.';
-  $string['nousercreated'] = 'Es wurde noch kein Benutzer erstellt.';
-  $string['createnewuser'] = 'Neuen Benutzer erstellen';
-  $string['editstudentdescription'] = 'Hier können Sie den ausgewälten Benutzer bearbeiten.';
-  $string['noclass'] = 'Keine Klasse';
-  $string['notalldata'] = 'Nicht alle Daten eingegeben';
-  $string['choosecohort'] = 'Wählen Sie eine Klasse um die Liste zu filtern';
-  $string['nousers'] = 'Noch kein Benutzer erstellt';
-  $string['toworktogether'] = 'Um mit anderen Schulen zusammenzuarbeiten';
-  $string['manageconnections'] = 'Mit Fachbereich verbinden';
-  $string['studentpagetitle'] = 'Schüler/Lehrer bearbeiten';
-  $string['connectionspagetitle'] = 'Verbindungen mit Fachbereichen bearbeiten';
-  $string['connections'] = 'Verbindungen mit Fachbereichen';
-  $string['connectionsdescription'] = 'Hier können Sie Ihre Schule mit südtirolweiten Fachbereichen verbinden. 
-		Somit kann schulübergreifend zusammengearbeitet werden.<br>
-		Sobald Sie eine Verbindung erstellt haben können Sie im Bearbeitungsmenü Ihrer Kurse den Bereich in 
-		"Fachbereiche->[Name des Fachs]->[name Ihrer Schule]" ändern. Somit kann von diesem Kurs aus dann auf schulübergreifende 
-		Ressourcen zugegriffen werden. Es wird empfohlen den Kurs zuerst im Bereich Ihrer Schule zu erstellen, die Benutzer hinzuzufügen und dann erst zu verschieben.
-		<br><b>Achtung</b> Erstellte Verbindungen können nicht mehr gelöscht werden. Erstellen Sie bitte nur Verbindungen die Sie auch benötigen.<br>';
- $string['connect'] = 'Mit Fachbereich verbinden';
- $string['connected'] = 'verbunden';
- $string['connectionalreadyexists'] = 'Bereits verbunden';
- $string['connectioncreated'] = 'Verbindung mit Fachbereich erstellt';
- $string['classmanager:manage'] = 'Verwalten';
- $string['enroledusers'] = 'eingeschriebene Benutzer';
- $string['areyousure'] = 'Sind Sie sicher?';
- $string['groupdeleted'] = 'Gruppe gelöscht!';
- $string['helpblock'] = 'Eine Kategorie wählen um mit der Verwaltung der Schule zu starten.';
-  $string['createcoursepagetitle'] = 'Kurs erstellen';
-  $string['coursenamedescription'] = 'Name des Kurses. Dieses sollte sehr aussagekräftig sein und das Fach/Thema, die betreffende Klasse und das Schuljahr enthalten. Bedenken Sie, dass der Kurs auch von den Schülern gefunden werden muss.';
-  $string['category'] = 'Speicherort';
-  $string['createcoursedescription'] = 'Wo soll der Kurs gespeichert werden? Bitte wählen Sie den betreffenden Fachbereich.';
-  $string['chooseclass'] = 'Klasse wählen';
-  $string['coursename'] = 'Name des Kurses';
-  $string['chooseclassdescription'] = 'Wählen Sie eine Gruppe, die in den Kurs eingeschrieben werden soll. Wenn Sie Einschreibungen erst später vornehmen möchten überspringen Sie diesen Schritt.';
-  $string['none'] = 'nichts ausgewählt';
-  $string['createcourse'] = 'neuen Kurs erstellen';
-  $string['emailchangenotice'] = 'Muss vom Nutzer selbst geändert werden';
+			<p>Nach dem Import erhalten Sie eine Liste mit den Passw&ouml;rtern. Bitte bewahren Sie diese sorgf&auml;ltig auf! Diese Liste kann nur ein Mal heruntergeladen werden!</p>';
+$string['iknowwhatido'] = 'Ich weiß was ich tue - Import starten';
+$string['wrongdata'] = 'Falsche Daten';
+$string['added'] = 'Hinzugef&uuml;gt';
+$string['classcreated'] = 'Klasse erstellt';
+$string['useralreadyexists'] = 'Benutzer existiert bereits';
+$string['classespagetitle'] = 'Verwaltung Ihrer Klassen';
+$string['classdescription'] = 'Hier k&ouml;nnen Sie die Klassen Ihrer Schule verwalten.';
+$string['editclassdescription'] = 'Hier k&ouml;nnen Sie eine Klasse bearbeiten.';
+$string['noclassescreated'] = 'Es wurden noch keine Klassen erstellt.';
+$string['newclass'] = 'Neue Klasse erstellen';
+$string['numberofstudents'] = 'Anzahl eingeschriebener<br>Benutzer.';
+$string['classnamenotconform'] = 'Klassenkurzname entspricht nicht den Regeln. Eine Zahl, dann Großbuchstaben. Ausnahmen: TE, PE';
+$string['studentsdescription'] = 'Hier k&ouml;nnen Sie Sch&uuml;ler und Lehrer Ihrer Schule verwalten. Klicken Sie auf einen Namen um ihn zu bearbeiten.';
+$string['nousercreated'] = 'Es wurde noch kein Benutzer erstellt.';
+$string['createnewuser'] = 'Neuen Benutzer erstellen';
+$string['editstudentdescription'] = 'Hier k&ouml;nnen Sie den ausgew&auml;lten Benutzer bearbeiten.';
+$string['noclass'] = 'Keine Klasse';
+$string['notalldata'] = 'Nicht alle Daten eingegeben';
+$string['choosecohort'] = 'W&auml;hlen Sie eine Klasse um die Liste zu filtern';
+$string['nousers'] = 'Noch kein Benutzer erstellt';
+$string['toworktogether'] = 'Um mit anderen Schulen zusammenzuarbeiten';
+$string['manageconnections'] = 'Mit Fachbereich verbinden';
+$string['studentpagetitle'] = 'Sch&uuml;ler/Lehrer bearbeiten';
+$string['connectionspagetitle'] = 'Verbindungen mit Fachbereichen bearbeiten';
+$string['connections'] = 'Verbindungen mit Fachbereichen';
+$string['connectionsdescription'] = 'Hier k&ouml;nnen Sie Ihre Schule mit s&uuml;dtirolweiten Fachbereichen verbinden. 
+		Somit kann schul&uuml;bergreifend zusammengearbeitet werden.<br>
+		Sobald Sie eine Verbindung erstellt haben k&ouml;nnen Sie im Bearbeitungsmen&uuml; Ihrer Kurse den Bereich in 
+		"Fachbereiche->[Name des Fachs]->[name Ihrer Schule]" &auml;ndern. Somit kann von diesem Kurs aus dann auf schul&uuml;bergreifende 
+		Ressourcen zugegriffen werden. Es wird empfohlen den Kurs zuerst im Bereich Ihrer Schule zu erstellen, die Benutzer hinzuzuf&uuml;gen und dann erst zu verschieben.
+		<br><b>Achtung</b> Erstellte Verbindungen k&ouml;nnen nicht mehr gel&ouml;scht werden. Erstellen Sie bitte nur Verbindungen die Sie auch ben&ouml;tigen.<br>';
+$string['connect'] = 'Mit Fachbereich verbinden';
+$string['connected'] = 'verbunden';
+$string['connectionalreadyexists'] = 'Bereits verbunden';
+$string['connectioncreated'] = 'Verbindung mit Fachbereich erstellt';
+$string['classmanager:manage'] = 'Verwalten';
+$string['enroledusers'] = 'eingeschriebene Benutzer';
+$string['areyousure'] = 'Sind Sie sicher?';
+$string['groupdeleted'] = 'Gruppe gel&ouml;scht!';
+$string['helpblock'] = 'Eine Kategorie w&auml;hlen um mit der Verwaltung der Schule zu starten.';
+$string['createcoursepagetitle'] = 'Kurs erstellen';
+$string['coursenamedescription'] = 'Name des Kurses. Dieses sollte sehr aussagekr&auml;ftig sein und das Fach/Thema, die betreffende Klasse und das Schuljahr enthalten. Bedenken Sie, dass der Kurs auch von den Sch&uuml;lern gefunden werden muss.';
+$string['category'] = 'Speicherort';
+$string['createcoursedescription'] = 'Wo soll der Kurs gespeichert werden? Bitte w&auml;hlen Sie den betreffenden Fachbereich.';
+$string['chooseclass'] = 'Klasse w&auml;hlen';
+$string['coursename'] = 'Name des Kurses';
+$string['chooseclassdescription'] = 'W&auml;hlen Sie eine Gruppe, die in den Kurs eingeschrieben werden soll. Wenn Sie Einschreibungen erst sp&auml;ter vornehmen m&ouml;chten &uuml;berspringen Sie diesen Schritt.';
+$string['none'] = 'nichts ausgew&auml;hlt';
+$string['createcourse'] = 'neuen Kurs erstellen';
+$string['emailchangenotice'] = 'Muss vom Nutzer selbst ge&auml;ndert werden';
