c6ff085a6d25cc330ca336ed0d658bb85f7bc58d
Mahlknecht Giovanni <giovanni.mahlknecht@inf.unibz.it>
2017-05-10 19:08:29 +0200

    removed unused stuff, Moodle coding guidelines

--- import.php
+++ import.php
@@ -1,8 +1,30 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-require_once("../../config.php"); //for Moodle integration
+/**
+ * Class manager for multiple schools
+ *
+ * @package block_classmanager
+ * @copyright 2017 Stefan Raffeiner, Giovanni Mahlknecht
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+require_once("../../config.php");
+require_login();
 require_once("config.php");
-$params = array();
+$params = array ();
 $PAGE->set_url('/my/index.php', $params);
 $PAGE->set_pagelayout('mydashboard');
 $PAGE->set_pagetype('my-index');
@@ -11,7 +33,7 @@ $context = context_user::instance($USER->id);
 $PAGE->set_context($context);
 $c = '';
 $header = get_string('importpagetitle', 'block_classmanager');
-if (!isset($_GET['category']) and ! isset($_POST['category'])) {
+if (! isset($_GET['category']) and ! isset($_POST['category'])) {
     $c .= "no category";
 } else {
     if (isset($_GET['category'])) {
@@ -19,56 +41,62 @@ if (!isset($_GET['category']) and ! isset($_POST['category'])) {
     } else {
         $categoryid = $_POST['category'];
     }
-
     $context = context_coursecat::instance($categoryid);
     if (has_capability(PERMISSION, $context)) {
-        $school = $DB->get_record('course_categories', array('id' => $categoryid));
-        $PAGE->navbar->add(get_string('manage', 'block_classmanager') . ' ' . $school->name, new moodle_url($CFG->wwwroot . '/blocks/classmanager/admin.php?category=' . $categoryid));
+        $school = $DB->get_record('course_categories', array (
+                'id' => $categoryid
+        ));
+        $desturl = new moodle_url($CFG->wwwroot . '/blocks/classmanager/admin.php?category=' . $categoryid);
+        $PAGE->navbar->add(get_string('manage', 'block_classmanager') . ' ' . $school->name, $desturl);
         $PAGE->navbar->add(get_string('import', 'block_classmanager'));
         if (isset($_FILES['importfile'])) {
             $file = fopen($_FILES['importfile']['tmp_name'], "r");
-            require '../../user/lib.php';
-            require '../../cohort/lib.php';
-            while (!feof($file)) {
+            require('../../user/lib.php');
+            require('../../cohort/lib.php');
+            while ( ! feof($file) ) {
                 $line = fgets($file, 2048);
                 $param = explode(';', $line);
                 if (count($param) == 5 || count($param) == 4) {
-                    $newuser = new stdClass;
-                    //FIXED UTF8 ISSUE
-                    for ($k = 1; $k < 3; $k++) {
-                        if (!mb_check_encoding($param[$k], 'UTF-8')) {
+                    $newuser = new stdClass();
+                    for ($k = 1; $k < 3; $k ++) {
+                        if (! mb_check_encoding($param[$k], 'UTF-8')) {
                             $param[$k] = mb_convert_encoding($param[$k], 'utf8');
                         }
                     }
                     $newuser->classe = trim(strtoupper($param[0]));
                     $newuser->lastname = trim($param[1]);
                     $newuser->firstname = trim($param[2]);
-
                     $newuser->mail = trim(strtolower($param[3]));
                     if (preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/", $newuser->mail)) {
-                        $class = $DB->get_record('cohort', array('idnumber' => $newuser->classe, 'contextid' => $context->id));
-                        if (!is_object($class)) {
-                            $newclass = new stdClass;
+                        $class = $DB->get_record('cohort', array (
+                                'idnumber' => $newuser->classe,
+                                'contextid' => $context->id
+                        ));
+                        if (! is_object($class)) {
+                            $newclass = new stdClass();
                             switch ($newuser->classe) {
-                                case 'TE':
+                                case 'TE' :
                                     $newclass->name = 'Lehrer';
                                     break;
-                                case 'PE':
+                                case 'PE' :
                                     $newclass->name = 'Personal';
                                     break;
-                                default:
+                                default :
                                     $newclass->name = $newuser->classe;
                             }
                             $newclass->idnumber = $newuser->classe;
                             $newclass->description = $school->name;
                             $newclass->contextid = $context->id;
                             $class = cohort_add_cohort($newclass);
-                            $c .= "<font color=\"green\">" . get_string('classcreated', 'block_classmanager') . ": " . $newuser->classe . "</font><br>";
+                            $c .= "<font color=\"green\">" . get_string('classcreated', 'block_classmanager') . ": ";
+                            $c .= $newuser->classe . "</font><br>";
                         }
-                        $class = $DB->get_record('cohort', array('idnumber' => $newuser->classe, 'contextid' => $context->id));
-
+                        $class = $DB->get_record('cohort', array (
+                                'idnumber' => $newuser->classe,
+                                'contextid' => $context->id
+                        ));
                         $pass = "Bz@" . rand(10000, 99999);
-                        $insertuser = new stdClass;
+                        $insertuser = new stdClass();
                         $insertuser->auth = 'manual';
                         $insertuser->confirmed = 1;
                         $insertuser->policyagreed = 0;
@@ -76,44 +104,45 @@ if (!isset($_GET['category']) and ! isset($_POST['category'])) {
                         $insertuser->password = $pass;
                         $insertuser->firstname = $newuser->firstname;
                         $insertuser->lastname = $newuser->lastname;
-                        #$insertuser->email = $newuser->mail;
                         $insertuser->mnethostid = 1;
                         $insertuser->lang = 'de';
-
-                        //test if user already exists
-                        //somewhere here is an error
-                        $olduser = $DB->get_record('user', array('username' => $insertuser->email));
+                        // Test if user already exists
+                        // somewhere here is an error!
+                        $olduser = $DB->get_record('user', array (
+                                'username' => $insertuser->email
+                        ));
                         if (is_object($olduser)) {
-                            $c .= "<b><font color=\"green\">" . get_string('useralreadyexists', 'block_classmanager') . ": " . $newuser->lastname . " " . $newuser->firstname . "</font></b><br>";
+                            $c .= "<b><font color=\"green\">" . get_string('useralreadyexists', 'block_classmanager') . ": ";
+                            $c .= $newuser->lastname . " " . $newuser->firstname . "</font></b><br>";
                         } else {
                             $user = user_create_user($insertuser);
-                            $user_cohort = cohort_add_member($class->id, $user);
+                            $usercohort = cohort_add_member($class->id, $user);
                             switch ($newuser->classe) {
-                                case 'TE':
+                                case 'TE' :
                                     $role = TEACHERROLE;
                                     break;
-                                case 'PE':
+                                case 'PE' :
                                     $role = NOTTEACHINGROLE;
                                     break;
-                                default:
+                                default :
                                     $role = STUDENTROLE;
                                     break;
                             }
-
                             $data = new stdClass();
                             $data->roleid = $role;
                             $data->userid = $user;
                             $data->timemodified = time();
                             $data->contextid = $context->id;
                             $roleassing = $DB->insert_record('role_assignments', $data);
-                            $c .= "" . get_string('added', 'block_classmanager') . ": 
-								" . $newuser->lastname . " " . $newuser->firstname . " 
-								" . get_string('username') . " " . $insertuser->username . " 
-								" . get_string('password') . " " . $pass . "<br>";
+                            $c .= "" . get_string('added', 'block_classmanager') . ": ";
+                            $c .= $newuser->lastname . " " . $newuser->firstname . " ";
+                            $c .= get_string('username') . " " . $insertuser->username . " ";
+                            $c .= get_string('password') . " " . $pass . "<br>";
                         }
                     } else {
-                        //this is not a correct user!
-                        $c .= "<b><font color=\"red\">" . get_string('wrongdata', 'block_classmanager') . ": " . $newuser->lastname . " " . $newuser->firstname . "</font></b><br>";
+                        // This is not a correct user!
+                        $c .= "<b><font color=\"red\">" . get_string('wrongdata', 'block_classmanager') . ": ";
+                        $c .= $newuser->lastname . " " . $newuser->firstname . "</font></b><br>";
                     }
                 }
             }
