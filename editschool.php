f4f14ca857e6830ee0b2e803e1e1e1773aa54daa
Mahlknecht Giovanni <giovanni.mahlknecht@inf.unibz.it>
2017-05-10 18:10:15 +0200

    Moodle coding guidelines

--- editschool.php
+++ editschool.php
@@ -1,13 +1,35 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-require_once("../../config.php"); //for Moodle integration
-require_once("config.php"); //for Classmanager Configuration
+/**
+ * Classmanager edit classes
+ *
+ * @package block_classmanager
+ * @copyright 2017 Stefan Raffeiner, Giovanni Mahlknecht
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+require_once("../../config.php");
+require_login();
+require_once("config.php");
 
-if (!isset($_GET['schoolid'])) {
+if (! isset($_GET['schoolid'])) {
     header('Location: ' . $CFG->wwwroot . '/blocks/classmanager/admin.php');
 }
 
-$params = array();
+$params = array ();
 $PAGE->set_url('/my/index.php', $params);
 $PAGE->set_pagelayout('mydashboard');
 $PAGE->set_pagetype('my-index');
@@ -21,8 +43,8 @@ $c = '';
 $course = $PAGE->course;
 $context = get_context_instance(CONTEXT_COURSE, $course->id);
 if (has_capability('moodle/user:editprofile', $context)) {
-    //SCHOOLS
-    $PAGE->navbar->add(get_string('manageschools', 'block_classmanager'), new moodle_url($CFG->wwwroot . '/blocks/classmanager/admin.php'));
+    $newurl = new moodle_url($CFG->wwwroot . '/blocks/classmanager/admin.php');
+    $PAGE->navbar->add(get_string('manageschools', 'block_classmanager'), $newurl);
     if (isset($_GET['name']) || isset($_GET['short']) || isset($_GET['manager'])) {
         if (isset($_GET['name']) && isset($_GET['short']) && isset($_GET['manager'])) {
             $school = new stdClass();
@@ -40,10 +62,10 @@ if (has_capability('moodle/user:editprofile', $context)) {
             $c .= get_string('notalldata', 'block_classmanager') . "<br>";
         }
     }
-
-
     if ($_GET['schoolid'] > 0) {
-        $school = $DB->get_record('classmanager_schools', array('id' => $_GET['schoolid']));
+        $school = $DB->get_record('classmanager_schools', array (
+                'id' => $_GET['schoolid']
+        ));
         $PAGE->navbar->add($school->name);
     } else {
         $school = new stdClass();
@@ -55,25 +77,31 @@ if (has_capability('moodle/user:editprofile', $context)) {
     }
     if (is_object($school)) {
         $c .= "<form action=\"editschool.php\"><input type=\"hidden\" name=\"schoolid\" value=\"" . $school->id . "\"><table>";
-        $c .= "<tr><td>" . get_string('shortname', 'block_classmanager') . "</td><td><input type=\"text\" name=\"short\" value=\"" . $school->short . "\"></td></tr>";
-        $c .= "<tr><td>" . get_string('name', 'block_classmanager') . "</td><td><input type=\"text\" name=\"name\" value=\"" . $school->name . "\"></td></tr>";
-        $users = $DB->get_records_sql('SELECT u.id, u.lastname, u.firstname 
-				FROM mdl_user u, mdl_role_assignments r
-				WHERE u.id = r.userid
-					AND r.roleid= ' . SCHOOLMANAGERROLE . '
-					AND r.contextid= 1
-				ORDER BY u.lastname, u.firstname');
+        $c .= "<tr><td>" . get_string('shortname', 'block_classmanager') . "</td><td><input type=\"text\" name=\"short\" value=\"";
+        $c .= $school->short . "\"></td></tr>";
+        $c .= "<tr><td>" . get_string('name', 'block_classmanager') . "</td><td><input type=\"text\" name=\"name\" value=\"";
+        $c .= $school->name . "\"></td></tr>";
+        $sqlstring = 'SELECT u.id, u.lastname, u.firstname ';
+        $sqlstring .= 'FROM mdl_user u, mdl_role_assignments r ';
+        $sqlstring .= "WHERE u.id = r.userid ";
+        $sqlstring .= "AND r.roleid= ' . SCHOOLMANAGERROLE . ' ";
+        $sqlstring .= "AND r.contextid= 1 ";
+        $sqlstring .= "ORDER BY u.lastname, u.firstname";
+        $users = $DB->get_records_sql($sqlstring);
         $c .= "<tr><td>" . get_string('manager', 'block_classmanager') . "</td><td>
 				<select size=\"1\"  name=\"manager\" >";
         foreach ($users as $user) {
             if ($user->id == $school->user) {
-                $c .= "<option value=\"" . $user->id . "\" selected=\"selected\">" . $user->lastname . " " . $user->firstname . "</option>";
+                $c .= "<option value=\"" . $user->id . "\" selected=\"selected\">" . $user->lastname . " ";
+                $c .= $user->firstname . "</option>";
             } else {
-                $c .= "<option value=\"" . $user->id . "\">" . $user->lastname . " " . $user->firstname . "</option>";
+                $c .= "<option value=\"" . $user->id . "\">" . $user->lastname . " " . $user->firstname;
+                $c .= "</option>";
             }
         }
         $c .= "</select></td></tr>";
-        $c .= "<tr><td><a href=\"" . $CFG->wwwroot . "/blocks/classmanager/admin.php\">" . get_string('back') . "</a></td><td><input type=\"submit\" value=\"" . get_string('submit') . "\"></td></tr>";
+        $c .= "<tr><td><a href=\"" . $CFG->wwwroot . "/blocks/classmanager/admin.php\">" . get_string('back');
+        $c .= "</a></td><td><input type=\"submit\" value=\"" . get_string('submit') . "\"></td></tr>";
         $c .= "</table></form>";
     } else {
         $c .= get_string('schoolnotfound', 'block_classmanager');
