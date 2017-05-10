a079dc577662909d35a0b25ce242ddbf9f450ecc
Mahlknecht Giovanni <giovanni.mahlknecht@inf.unibz.it>
2017-05-10 16:31:41 +0200

    Moodle coding guidelines

--- admin.php
+++ admin.php
@@ -1,7 +1,31 @@
 <?php
-require_once("../../config.php");
-require_once("./config.php");
-$params = array();
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Class Manager school administration
+ *
+ * @package contrib
+ * @subpackage block_classmanager
+ * @copyright 2017 Stefan Raffeiner, Giovanni Mahlknecht
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+require_once ("../../config.php");
+require_login();
+require_once ("./config.php");
+$params = array ();
 $PAGE->set_url('/my/index.php', $params);
 $PAGE->set_pagelayout('mydashboard');
 $PAGE->set_pagetype('my-index');
@@ -12,30 +36,55 @@ $context = context_user::instance($USER->id);
 $PAGE->set_context($context);
 
 $c = '';
-if (!isset($_GET['category'])) {
+if (! isset($_GET['category'])) {
     $c .= "no category";
 } else {
     $context = context_coursecat::instance($_GET['category']);
     if (has_capability(PERMISSION, $context)) {
-        $school = $DB->get_record('course_categories', array('id' => $_GET['category']));
-        $PAGE->navbar->add(get_string('manage', 'block_classmanager') . ' ' . $school->name); //, new moodle_url('/a/link/if/you/want/one.php'));
-        $numberofclasses = $DB->count_records('cohort', array('contextid' => $context->id));
+        $school = $DB->get_record('course_categories', array (
+                'id' => $_GET['category']
+        ));
+        $PAGE->navbar->add(get_string('manage', 'block_classmanager') . ' ' . $school->name);
+        $numberofclasses = $DB->count_records('cohort', array (
+                'contextid' => $context->id
+        ));
         $numberofstudents = 0;
-        foreach ($DB->get_records('cohort', array('contextid' => $context->id)) as $class) {
-            $numberofstudents += $DB->count_records('cohort_members', array('cohortid' => $class->id));
+        foreach ($DB->get_records('cohort', array (
+                'contextid' => $context->id
+        )) as $class) {
+            $numberofstudents += $DB->count_records('cohort_members', array (
+                    'cohortid' => $class->id
+            ));
         }
         $c .= get_string('managerpagedescription', 'block_classmanager');
-        $c .= "<table>
-			<tr><td><a href=\"" . $CFG->wwwroot . "/blocks/classmanager/import.php?category=" . $_GET['category'] . "\">" . get_string('importstudents', 'block_classmanager') . "</a></td>
-				<td>" . get_string('theeaysiestway', 'block_classmanager') . "</td></tr>
-			<tr><td><a href=\"" . $CFG->wwwroot . "/blocks/classmanager/classes.php?category=" . $_GET['category'] . "\">" . get_string('manageclasses', 'block_classmanager') . "</a></td>
-				<td>" . get_string('youhave', 'block_classmanager') . " " . $numberofclasses . " " . get_string('classes', 'block_classmanager') . "</td></tr>
-			<tr><td><a href=\"" . $CFG->wwwroot . "/blocks/classmanager/students.php?category=" . $_GET['category'] . "\">" . get_string('managestudents', 'block_classmanager') . "</a></td>
-				<td>" . get_string('youhave', 'block_classmanager') . " " . $numberofstudents . " " . get_string('students', 'block_classmanager') . "</td></tr>
-			<tr><td><a href=\"" . $CFG->wwwroot . "/blocks/classmanager/deleteusers.php?category=" . $_GET['category'] . "\">" . get_string('deleteusers', 'block_classmanager') . "</a></td>
-				<td>" . get_string('deleteusersmanydescription', 'block_classmanager') . "</td></tr>
-			<tr><td><a href=\"" . $CFG->wwwroot . "/blocks/classmanager/connections.php?category=" . $_GET['category'] . "\">" . get_string('manageconnections', 'block_classmanager') . "</a></td>
-				<td>" . get_string('toworktogether', 'block_classmanager') . "</td></tr>";
+        $c .= "<table>";
+        $c .= "<tr>";
+        $c .= "<td><a href=\"" . $CFG->wwwroot . "/blocks/classmanager/import.php?category=" . $_GET['category'] . "\">";
+        $c .= get_string('importstudents', 'block_classmanager') . "</a></td>";
+        $c .= "<td>" . get_string('theeaysiestway', 'block_classmanager') . "</td>";
+        $c .= "</tr>";
+        $c .= "<tr>";
+        $c .= "<td><a href=\"" . $CFG->wwwroot . "/blocks/classmanager/classes.php?category=" . $_GET['category'] . "\">";
+        $c .= get_string('manageclasses', 'block_classmanager') . "</a></td>";
+        $c .= "<td>" . get_string('youhave', 'block_classmanager') . " " . $numberofclasses . " ";
+        $c .= get_string('classes', 'block_classmanager') . "</td>";
+        $c .= "</tr>";
+        $c .= "<tr>";
+        $c .= "<td><a href=\"" . $CFG->wwwroot . "/blocks/classmanager/students.php?category=" . $_GET['category'] . "\">";
+        $c .= get_string('managestudents', 'block_classmanager') . "</a></td>";
+        $c .= "<td>" . get_string('youhave', 'block_classmanager') . " " . $numberofstudents . " ";
+        $c .= get_string('students', 'block_classmanager') . "</td>";
+        $c .= "</tr>";
+        $c .= "<tr>";
+        $c .= "<td><a href=\"" . $CFG->wwwroot . "/blocks/classmanager/deleteusers.php?category=" . $_GET['category'] . "\">";
+        $c .= get_string('deleteusers', 'block_classmanager') . "</a></td>";
+        $c .= "<td>" . get_string('deleteusersmanydescription', 'block_classmanager') . "</td>";
+        $c .= "</tr>";
+        $c .= "<tr>";
+        $c .= "<td><a href=\"" . $CFG->wwwroot . "/blocks/classmanager/connections.php?category=" . $_GET['category'] . "\">";
+        $c .= get_string('manageconnections', 'block_classmanager') . "</a></td>";
+        $c .= "<td>" . get_string('toworktogether', 'block_classmanager') . "</td>";
+        $c .= "</tr>";
         $c .= "</table>";
     } else {
         $c .= 'Ich root - du nix';
